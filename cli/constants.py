ddcde37fd8fd9f6012c9ec0b5da804407d612bfd
Henny Sipma <hennysipma@mac.com>
2026-04-12 19:35:50 -0700

    codehawk: update hash

--- cli/constants.py
+++ cli/constants.py
@@ -12,7 +12,7 @@ WANT = {
     # Note that 10j-more-deps builds against a specific version of LLVM, so before
     # upgrading the major version of 10j-llvm, update 10j-more-deps first.
     "10j-more-deps": "rev-d915905d0",
-    "10j-codehawk": "5414e06ea6461c759fe565db14e5c8ff64047725",
+    "10j-codehawk": "c25aab382efee1c435ab83e2ee0f4ca12dfaa1d9",
     "10j-codehawk-c": "db010d1bab5381f9986dbf269ead9c036b55df07",
     # Keep in sync with the version in `xj-improve-multitool/rust-toolchain.toml`.
     "10j-xj-improve-multitool-toolchain": "nightly-2025-08-20",
